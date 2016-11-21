032f278eea6fceb6f0b6a60ee4a103f1756587f2
ColdNightHour <jdixo001@ucr.edu>
2016-11-20 19:38:49 -0800

    Began environment creation for the web view

---
+++ script.sh
@@ -0,0 +1,4 @@
+cd /home/coldnighthour/Documents/CS172/CS172-Tweet-Collector/
+mvn clean install
+mvn exec:java -Dexec.mainClass="Part2.QueryLucene"
+cd /home/coldnighthour/Documents/CS172/TwitterCrawlerWeb/ 
